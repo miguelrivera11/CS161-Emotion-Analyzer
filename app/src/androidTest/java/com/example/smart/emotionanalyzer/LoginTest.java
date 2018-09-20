41bdcdd38791eb113f4420a870f0f29b2e608328
Albert Yu <albertyu_85@yahoo.com>
2018-09-19 22:04:43 -0700

    added more test cases

--- app/src/androidTest/java/com/example/smart/emotionanalyzer/LoginTest.java
+++ app/src/androidTest/java/com/example/smart/emotionanalyzer/LoginTest.java
@@ -17,7 +17,13 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -42,7 +48,36 @@ public class LoginTest {
         onView(withId(R.id.password)).perform(click()).perform(replaceText("Tester123"), closeSoftKeyboard());
         onView(withId(R.id.email_sign_in_button)).perform(click());
         Activity mainActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 3000);
+        onView(withId(R.id.logout)).perform(click());
         mainActivity.finish();
+    }
 
+    @Test
+    public void incorrectEmail() {
+        onView(withId(R.id.email)).perform(click()).perform(replaceText("incorrectemail@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(click()).perform(replaceText("Tester123"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withText("Authentication failed.")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+   @Test
+    public void blankPassword() {
+        onView(withId(R.id.email)).perform(click()).perform(replaceText("incorrectemail@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(click()).perform(replaceText(""), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+    }
+
+    @Test
+    public void blankEmail() {
+        onView(withId(R.id.email)).perform(click()).perform(replaceText(""), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(click()).perform(replaceText("Tester123"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+    }
+    @Test
+    public void incorrectPassword() {
+        onView(withId(R.id.email)).perform(click()).perform(replaceText("test@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(click()).perform(replaceText("Tester"), closeSoftKeyboard());
+        onView(withId(R.id.email_sign_in_button)).perform(click());
+        onView(withText("Authentication failed.")).inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 }
