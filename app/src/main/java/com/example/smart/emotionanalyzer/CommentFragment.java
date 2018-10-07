ce5850c6e16191070b35ab3e808c46f8c9d24580
Albert Yu <albertyu_85@yahoo.com>
2018-10-07 13:04:21 -0700

    finished layout of comment/reply structure

--- app/src/main/java/com/example/smart/emotionanalyzer/CommentFragment.java
+++ app/src/main/java/com/example/smart/emotionanalyzer/CommentFragment.java
@@ -14,6 +14,7 @@ import android.support.annotation.NonNull;
 
 import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 
@@ -27,21 +28,70 @@ import com.google.firebase.database.ValueEventListener;
 import android.app.ProgressDialog;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class CommentFragment extends Fragment {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expandableListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_comment, null);
+        expandableListView = (ExpandableListView) view.findViewById(R.id.lvExp);
+
+        prepareListData();
         EditText comment = (EditText) view.findViewById(R.id.editTextComment);
 
+        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
+        expandableListView.setAdapter(listAdapter);
         return view;
     }
 
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
+        // Adding child data
+        listDataHeader.add("Top 250asdfaslkdfj;asdkjf;alsdkjf;laskdjf;laskjdf;lakjdf;laskjdf;laskjdf;lakjdf;lajsdkfl;jas;dlfjkas;ldfjka;lsdjkf;alsdf");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 
 }
