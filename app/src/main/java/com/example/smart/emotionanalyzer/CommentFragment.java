9ac450a770a2043dd9d7bcb000c4e5d89430033e
Albert Yu <albertyu_85@yahoo.com>
2018-10-07 21:52:09 -0700

    fixed value listener

--- app/src/main/java/com/example/smart/emotionanalyzer/CommentFragment.java
+++ app/src/main/java/com/example/smart/emotionanalyzer/CommentFragment.java
@@ -5,6 +5,7 @@ import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -82,13 +83,11 @@ public class CommentFragment extends Fragment {
                             userObject.addCommentedTopic(a.getTopic());
                             usersRef.setValue(userObject);
                         }
-
                         @Override
                         public void onCancelled(DatabaseError databaseError) {
 
                         }
                     });
-
                     topicsRef.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
@@ -102,21 +101,19 @@ public class CommentFragment extends Fragment {
                                 }
                             }
                         }
-
                         @Override
                         public void onCancelled(DatabaseError databaseError) {
 
                         }
                     });
-                            //Comment comment = new Comment("TEST COMMENT", "Random Person", "MadeUpID", "9/29/18");
                     Toast.makeText(getActivity(), commentEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+                    comments = new ArrayList<>();
                     commentEditText.setText("");
                     return true;
                 }
                 return false;
             }
         });
-
 
         topicsRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -163,39 +160,7 @@ public class CommentFragment extends Fragment {
             counter++;
         }
 
-        // Adding parent data(
-        /*listDataHeader.add("Top 250asdfaslkdfj;asdkjf;alsdkjf;laskdjf;laskjdf;lakjdf;laskjdf;laskjdf;lakjdf;lajsdkfl;jas;dlfjkas;ldfjka;lsdjkf;alsdf");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);*/
     }
+
 
 }
